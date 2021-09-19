75a941cb141f9c0d9f8f78f29b380d094d117a9c
SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2021-09-19 18:22:41 +0900

    add security group description

--- terraform/securitygroup.tf
+++ terraform/securitygroup.tf
@@ -5,21 +5,23 @@ resource "aws_security_group" "lb" {
 
   ingress = [
     {
-      description = "HTTP"
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+      description      = "HTTP"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = null
     }
   ]
 
   egress = [
     {
+      description      = "egress"
       from_port        = 0
       to_port          = 0
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
+      ipv6_cidr_blocks = null
     }
   ]
 
