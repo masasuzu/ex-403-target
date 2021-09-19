108ab41afb4f34c980747732feac936f8d742bb2
SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2021-09-19 17:34:04 +0900

    add public route

--- terraform/network.tf
+++ terraform/network.tf
@@ -35,7 +35,21 @@ resource "aws_subnet" "private" {
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
+  tags = local.tags
+}
+
+# Route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route = [
+    {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.main.id
+    }
+  ]
+
   tags = {
-    Name = local.name
+    Name = "${local.name}-public"
   }
 }
