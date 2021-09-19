5c882880a65830aa25cc0695869245cb184c1cd2
SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2021-09-19 18:18:55 +0900

    add ALB

---
+++ terraform/loadblancer.tf
@@ -0,0 +1,39 @@
+# ALB
+resource "aws_lb" "main" {
+  name               = local.name
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb.id]
+  subnets            = aws_subnet.public.*.id
+
+  tags = local.tags
+}
+
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "main" {
+  listener_arn = aws_lb_listener.main.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+
+resource "aws_lb_target_group" "main" {
+  name        = local.name
+  port        = 80
+  protocol    = "HTTPS"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+}
---
+++ terraform/securitygroup.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "lb" {
+  name        = local.name
+  description = local.name
+  vpc_id      = aws_vpc.main.id
+
+  ingress = [
+    {
+      description = "HTTP"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
+
+  tags = local.tags
+}
+
