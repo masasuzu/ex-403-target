65f28b2c158ccffe93e1c7da30af55b2b9cf2162
SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2021-09-19 18:52:06 +0900

    こう?

--- terraform/loadblancer.tf
+++ terraform/loadblancer.tf
@@ -4,7 +4,7 @@ resource "aws_lb" "main" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb.id]
-  subnets            = aws_subnet.public.*.id
+  subnets            = aws_subnet.public[*].id
 
   tags = local.tags
 }
