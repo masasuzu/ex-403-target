dca4bfdfbf64a85c8215ad32f6e57678b1f1fbbe
SUZUKI, Masashi <ma.suzuki@bengo4.com>
2021-09-22 17:48:00 +0900

    natの場合の紐付けはrouteでいいらしい

--- terraform/network.tf
+++ terraform/network.tf
@@ -71,9 +71,10 @@ resource "aws_route_table" "private" {
   }
 }
 
-resource "aws_route_table_association" "private_gateway" {
-  gateway_id     = aws_nat_gateway.main.id
-  route_table_id = aws_route_table.private.id
+resource "aws_route" "private_nat" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.main.id
 }
 
 # Nat
