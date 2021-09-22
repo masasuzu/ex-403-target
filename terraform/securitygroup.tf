5c8a97f66a9793eeb00bc2568260e054a84043f3
SUZUKI, Masashi <ma.suzuki@bengo4.com>
2021-09-22 20:21:03 +0900

    lbはhttpだったのだ

--- terraform/securitygroup.tf
+++ terraform/securitygroup.tf
@@ -5,9 +5,9 @@ resource "aws_security_group" "lb" {
 
   ingress = [
     {
-      description      = "HTTPS"
-      from_port        = 443
-      to_port          = 443
+      description      = "HTTP"
+      from_port        = 80
+      to_port          = 80
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = null
