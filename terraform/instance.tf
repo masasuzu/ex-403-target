c893e17fb137f93d9c8bdb62bba6f9bba35a6106
SUZUKI, Masashi <ma.suzuki@bengo4.com>
2021-09-22 19:14:07 +0900

    userdataを使うように

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -21,5 +21,19 @@ resource "aws_instance" "main" {
   subnet_id            = aws_subnet.private["ap-northeast-1a"].id
   iam_instance_profile = aws_iam_instance_profile.ec2.name
 
+  user_data = <<EOL
+#cloud-config
+package_update: true
+package_upgrade: true
+timezone: Asia/Tokyo
+hostname: ${local.name}
+packages:
+  - nginx
+runcmd:
+  - [ snap, switch, --channel=candidate, amazon-ssm-agent ]
+# DEBUG
+output : { all : '| tee -a /var/log/cloud-init-output.log' }
+EOL
+
   tags = local.tags
 }
